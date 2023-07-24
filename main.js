4b8fb57fce4f9be375652a52d8a382d264edcd71
Pitan <58260965+PTOM76@users.noreply.github.com>
2023-07-24 22:55:55 +0900

    update

---
+++ main.js
@@ -0,0 +1,8 @@
+function age(ymd){
+    $today = new Date();
+    $y = $today.getYear(); if($y < 1900) $y += 1900;
+    $m = $today.getMonth() + 1;
+    $d = $today.getDate();
+    $today = $y * 10000 + $m * 100 + $d;
+    document.write(Math.floor(($today - ymd) / 10000));
+}
