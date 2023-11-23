ffb56db8f16060bef2fbb304d6fd4d1d5b651174
Pitan <58260965+PTOM76@users.noreply.github.com>
2023-11-23 14:23:11 +0900

    update

---
+++ tools/regexp.js
@@ -0,0 +1,46 @@
+function preg_match(pattern, subject, matches = null) {
+    let patternMatches;
+    let flags = "";
+    if (patternMatches = pattern.match("^\/(.*)\/([a-zA-Z]*)$")) {
+        flags = patternMatches[2];
+        pattern = patternMatches[1];
+    }
+    const regex = new RegExp(pattern, flags);
+    const result = subject.match(regex);
+    if (matches !== null) {
+        // ...演算子で配列を展開してpushする (matches.push(result[0], result[1], ...) と同じ)
+        matches.push(...result);
+    }
+    return result !== null ? result.length : false;
+}
+
+function regexp_checkbtn() {
+    let $result_tag = document.getElementById("result");
+    $result_tag.innerHTML = "";
+
+    let $pattern = document.getElementById("pattern").value;
+    let $subject = document.getElementById("subject").value;
+    let $matches = [];
+
+    let $result = preg_match($pattern, $subject, $matches);
+
+    let $output = "";
+
+    if (!$pattern.match("^\/.*\/[a-zA-Z]*$")) $pattern = "/" + $pattern + "/";
+    $output += "PHPコード: " + "<input id=\"php_code\" style=\"width:100%;height:22px; font-size: 15px;\" value=\"preg_match('" + $pattern + "', '" + $subject + "', $matches);\" disabled />" + "<br />";
+    $output += "JSコード: " + "<input id=\"js_code\" style=\"width:100%;height:22px; font-size: 15px;\" value=\"$matches = '" + $subject + "'.match(" + $pattern + ");\" disabled />" + "<br /><br />";
+
+    if ($result) {
+        $output += "マッチしました。<br />";
+        $output += "マッチした回数: " + $result + "<br />";
+        if ($matches.length > 0) {
+            $output += "マッチした文字列: <br />"
+            $matches.forEach(function($match, $index) { 
+                $output += "$matches[" + $index + "]: " + $match + "<br />";
+            });
+        }
+    } else {
+        $output += "マッチしませんでした。<br />";
+    }
+    $result_tag.innerHTML = $output;
+}
