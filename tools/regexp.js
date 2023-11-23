4a4cb67e23b9aff1a2856c653beee6963ad1d088
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2023-11-23 15:19:47 +0900

    aaaa

--- tools/regexp.js
+++ tools/regexp.js
@@ -1,3 +1,4 @@
+// License: CC0-1.0 (https://creativecommons.org/publicdomain/zero/1.0/deed.ja)
 function preg_match(pattern, subject, matches = null) {
     let patternMatches;
     let flags = "";
